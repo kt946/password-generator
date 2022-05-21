57f743c7de76aca5929847e1bd50195a58375e54
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-05-21 00:36:10 -0400

    added functions for user input

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+lowerCase = "abcdefghijklmnopqrstuvwxyz";
 
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-var numericChar = "1234567890";
+anumericChar = "1234567890";
 
-var specialChar = " !'#$%&()*+,-./:;<=>?@[\]^_`{|}~"+'"';
+specialChar = " !'#$%&()*+,-./:;<=>?@[\]^_`{|}~"+'"';
+
+passwordLength = "";
+
+passwordArray = "";
 
 var randomNum = function(min, max) {
   value = Math.floor(Math.random() * (max - min + 1));
@@ -18,24 +22,56 @@ var randomNum = function(min, max) {
 var getLengthPw = function () {
   // ask user for password length
   var lengthInput = window.prompt("Specify the length of your password. It must be at least 8 characters and no more than 128 characters.");
-  
-      if (lengthInput > 8 && lengthInput < 128) {
-          return lengthInput;
-      }
-      // if user input a blank or special character or length did not fit requirements
-      else {
-          window.alert("You did not input a valid length! Please try again!");
-          getLengthPw();
-      }
+    if (lengthInput > 8 && lengthInput < 128) {
+        return lengthInput;
+    }
+    // if user input a blank or special character or length did not fit requirements
+    else {
+        window.alert("You did not input a valid length! Please try again!");
+        getLengthPw();
+    }
   };
 
-var confirmLowerCase = window.confirm("Would you like to include lowercase characters?");
+// function to ask for lowercase characters
+var addLowerCase = function() {
+  var confirmLowerCase = window.confirm("Would you like to include lowercase characters?");
+    if (confirmLowerCase) {
+      passwordArray += lowerCase;
+    }
+};
+
+// function to ask for uppercase characters
+var addUpperCase = function() {
+  var confirmUpperCase = window.confirm("Would you like to include uppercase characters?");
+    if (confirmUpperCase) {
+      passwordArray += upperCase;
+    }
+};
+
+// function to ask for numeric characters
+var addNumChar = function() {
+  var confirmNumChar = window.confirm("Would you like to include numeric characters?");
+    if (confirmNumChar) {
+      passwordArray += numericChar;
+    }
+};
+
+// function to ask for special characters
+var addSpecialChar = function() {
+  var confirmSpecialChar = window.confirm("Would you like to include special characters?");
+    if (confirmNumChar) {
+      passwordArray += specialChar;
+    }
+};
 
 function generatePassword() {
   // add your logic here
-
+  passwordArray = "";
+  lengthInput = "";
   // Prompt user for the length of the password (8 - 128)
+  getLengthPw();
   // Prompt user if they want to include lowercase characters
+
   // Prompt user if they want to include uppercase characters
   // Prompt user if they want to include numeric characters
   // Prompt user if they want to include special characters
