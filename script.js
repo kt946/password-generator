1a8ed7576d52c1933af96bb8c8f7b96c86141e2b
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-05-21 07:43:22 -0400

    added functions to generatePassword()

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ lowerCase = "abcdefghijklmnopqrstuvwxyz";
 
 upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-anumericChar = "1234567890";
+numericChar = "1234567890";
 
-specialChar = " !'#$%&()*+,-./:;<=>?@[]^_`{|}~"+'"';
+specialChar = " !'#$%&()*+,-./:;<=>?@[\\]^_`{|}~"+'"';
 
 var getNewPassword = function() {
     for (var i = 0; i < passwordLength; i ++ ) {
@@ -18,7 +18,7 @@ var getNewPassword = function() {
 };
 
 // function to input length of password
-var getLengthPw = function () {
+var getLength = function () {
   // ask user for password length
   var lengthInput = window.prompt("Specify the length of your password. It must be at least 8 characters and no more than 128 characters.");
     if (lengthInput >= 8 && lengthInput <= 128) {
@@ -27,7 +27,7 @@ var getLengthPw = function () {
     // if user input a blank or special character or length did not fit requirements
     else {
         window.alert("You did not input a valid length! Please try again!");
-        getLengthPw();
+        getLength();
     }
   };
 
@@ -58,36 +58,39 @@ var addNumChar = function() {
 // function to ask for special characters
 var addSpecialChar = function() {
   var confirmSpecialChar = window.confirm("Would you like to include special characters?");
-    if (confirmNumChar) {
+    if (confirmSpecialChar) {
       passwordArray += specialChar;
     }
 };
 
-// condition if none of the critera were selected
-if (passwordArray === "") {
-  window.alert("You need to select at least one character type to proceed.")
-}
-
 function generatePassword() {
   // add your logic here
+  // reset variables for new password
   passwordLength = "";
   passwordArray = "";
   newPassword = "";
 
   // Prompt user for the length of the password (8 - 128)
-  getLengthPw();
+  passwordLength = getLength();
   // Prompt user if they want to include lowercase characters
- 
+  addLowerCase();
   // Prompt user if they want to include uppercase characters
+  addUpperCase();
   // Prompt user if they want to include numeric characters
+  addNumChar();
   // Prompt user if they want to include special characters
+  addSpecialChar();
   // Validate input (between 8 - 128 characters, at least one type must be selected)
+  if (passwordArray === "") {
+    window.alert("You need to select at least one character type to proceed! Please try again!");
+    generatePassword();
+  };
   // Generate password
+  newPassword = getNewPassword();
 
   // Display password to the page
-  
-  return;
-}
+  return newPassword;
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -96,7 +99,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-}
+  debugger
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
