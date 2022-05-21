7e0d52c6a904fd565346b3b121a268f2a3a96345
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-05-21 00:07:27 -0400

    changed arrays into strings

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 
-var upperCase =  [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",  "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-var numericChar = ["0", "1", "2", "3","4", "5", "6", "7", "8", "9"];
+var numericChar = "1234567890";
 
-var specialChar = [ ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
+var specialChar = " !'#$%&()*+,-./:;<=>?@[\]^_`{|}~"+'"';
 
 var randomNum = function(min, max) {
-  value = Math.floor(Math.random() * (max - min + 1)) + min;
+  value = Math.floor(Math.random() * (max - min + 1));
   return value;
 };
 
@@ -29,6 +29,7 @@ var getLengthPw = function () {
       }
   };
 
+var confirmLowerCase = window.confirm("Would you like to include lowercase characters?");
 
 function generatePassword() {
   // add your logic here
